95c41f3e41863a41dd6bd028d1ea5a5d1b5269bf
Charles Dunn <dunnc1551@gmail.com>
2017-05-30 10:55:22 -0600

    Add actions to fetch sharks and pings from internal api.

--- client/src/actions/actions.js
+++ client/src/actions/actions.js
@@ -1,6 +1,29 @@
-export const getUsers = (user) => {
+export const addSharks = (sharks) => {
   return {
-    type: 'GET_USERS',
-    user
+    type: 'ADD_SHARKS',
+    sharks
   };
 };
+
+export const addPings = (pings) => {
+  return {
+    type: 'ADD_PINGS',
+    pings
+  }
+}
+
+export const fetchSharks = () => {
+  return (dispatch) => {
+    fetch('/api/v1/sharks')
+      .then(response => response.json())
+      .then(sharks => dispatch(addSharks(sharks)))
+  }
+}
+
+export const fetchPings = () => {
+  return (dispatch) => {
+    fetch(`/api/v1/pings`)
+      .then(response => response.json())
+      .then(pings => dispatch(addPings(pings)))
+  }
+}
