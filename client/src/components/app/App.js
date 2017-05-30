01fd857adf16c9070940928b1eda8b85b5d9c39b
Charles Dunn <dunnc1551@gmail.com>
2017-05-30 10:56:58 -0600

    Add renderPings method to display shark pings by shark.

--- client/src/components/app/App.js
+++ client/src/components/app/App.js
@@ -1,53 +1,46 @@
 import React, { Component } from 'react';
-import { Map, Marker, Popup, TileLayer } from 'react-leaflet';
+import { Map, TileLayer } from 'react-leaflet';
+import { SharkMarker } from '../marker/marker';
 import './App.css';
-import L from 'leaflet';
-
-L.Icon.Default.imagePath = '.';
-delete L.Icon.Default.prototype._getIconUrl;
-
-L.Icon.Default.mergeOptions({
-  iconRetinaUrl: require('leaflet/dist/images/marker-icon-2x.png'),
-  iconUrl: require('leaflet/dist/images/marker-icon.png'),
-  shadowUrl: require('leaflet/dist/images/marker-shadow.png'),
-});
-
 
 const position = [51.505, -0.09];
 
-class App extends Component {
-  state = {users: []}
+export default class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+    }
+  }
 
   componentDidMount() {
-    fetch('/users')
-      .then(res => res.json())
-      .then(users => this.setState({ users }));
+    this.props.fetchSharks();
+    this.props.fetchPings();
+  }
+
+  renderPings() {
+    return this.props.pings.map((ping, i) => {
+      if (i < 500) {
+        return (
+          <SharkMarker
+          key={i}
+          id = { ping.shark_id }
+          lat={ parseFloat(ping.latitude) }
+          lng={ parseFloat(ping.longitude) }
+          datetime={ ping.datetime }
+          />
+        )
+      }
+    });
   }
 
   render() {
     return (
       <div className="App">
-        <Map center={position} zoom={2}>
-          <TileLayer url='http://server.arcgisonline.com/arcgis/rest/services/Ocean/World_Ocean_Base/MapServer/tile/{z}/{y}/{x}'/>
-          <Marker position={position}>
-            <Popup>
-              <span>A pretty CSS3 <a href='google.com'>click here</a> popup.<br/>Easily customizable.</span>
-            </Popup>
-          </Marker>
-          <Marker position={[20, -20]}>
-            <Popup>
-              <span>SUP DFJDKFJS</span>
-            </Popup>
-          </Marker>
-
+        <Map center={ position } zoom={2}>
+          <TileLayer url='http://server.arcgisonline.com/arcgis/rest/services/World_Imagery/MapServer/tile/{z}/{y}/{x}'/>
+          { this.renderPings() }
         </Map>
-        <h1>Users</h1>
-        {this.state.users.map(user =>
-          <div key={user.id}>{user.username}</div>
-        )}
       </div>
     );
   }
 }
-
-export default App;
