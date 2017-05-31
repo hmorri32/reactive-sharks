48d6d93726cbeff68afd927e99e495f1a002ee3d
Charles Dunn <dunnc1551@gmail.com>
2017-05-31 08:54:21 -0600

    Render additional shark props.

--- client/src/components/marker/marker.js
+++ client/src/components/marker/marker.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { Marker, Popup } from 'react-leaflet';
 import L from 'leaflet';
+import { Button } from '../button/button';
 
 L.Icon.Default.imagePath = '.';
 delete L.Icon.Default.prototype._getIconUrl;
@@ -12,15 +13,20 @@ L.Icon.Default.mergeOptions({
 });
 
 
-export const SharkMarker = ({ name, id, lat, lng, datetime }) => {
+export const SharkMarker = ({ name, species, length, weight, gender, id, lat, lng, datetime }) => {
   return (
     <Marker position={[lat, lng]}>
       <Popup>
         <div>
           <h3>Name: { name }</h3>
+          <p>Species: { species }</p>
+          <p>Length: { length }</p>
+          <p>Weight: { weight }</p>
+          <p>Gender: { gender }</p>
           <p>Date: { datetime }</p>
           <p>Latitude: { lat }</p>
           <p>Longitude: { lng }</p>
+          <Button btnName='Track Shark' />
         </div>
      </Popup>
     </Marker>
