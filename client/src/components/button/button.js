ca2dc9309a0f93c73e07d124904c754d26513d66
Charles Dunn <dunnc1551@gmail.com>
2017-06-03 17:12:05 -0600

    Add span element for button hover state.

--- client/src/components/button/button.js
+++ client/src/components/button/button.js
@@ -1,13 +1,19 @@
 import React from 'react';
 
-export const Button = ({ btnName, handleClick, className }) => {
+export const Button = ({ btnName, handleClick, className, icon, id }) => {
   return (
-    <button
-      className={ className }
-      onClick={ (e) => handleClick(e) }
-      value={ btnName }
-    >
-    { btnName }
-    </button>
+    <div className='map-toggle-wrap'>
+      <span className='map-type'>{ id }</span>
+      <button
+        className={ className }
+      >
+        <img
+          onClick={ (e) => handleClick(e) }
+          id={ id }
+          className='map-toggle-btn'
+          src={ icon }
+        />
+      </button>
+    </div>
   )
 }
