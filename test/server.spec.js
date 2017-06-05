5ba063196484a566b3820c62f1a2e40133445bf5
Hugh Morrison <hmorri32@gmail.com>
2017-06-05 15:12:36 -0600

    set up server.spec.js file for backend testing

--- test/server.spec.js
+++ test/server.spec.js
@@ -16,5 +16,47 @@ chai.use(chaiHttp);
 describe('testing our yung sharks', function() {
   it('should exist', () => {
     expect(server).to.exist;
+  });
+});
+
+describe('server side testing', () => {
+
+  before((done) => {
+    database.migrate.latest()
+    .then(() => database.seed.run())
+    .then(() => done());
+  });
+
+  afterEach((done) => {
+    database.seed.run()
+    .then(() => done());
+  });
+
+  describe('client routes', () => {
+    it(' / should render HTML', (done) => {
+      chai.request(server)
+      .get('/')
+      .end((error, response) => {
+        response.should.have.status(200)
+        response.text.should.equal('<!DOCTYPE html><html><head><title>Express</title></head><body><h1>Express</h1><p>Welcome to Express</p></body></html>');
+        done();
+      });
+    });
+
+    it('should render HTML error for bogus route', () => {
+      chai.request(server)
+      .get('/ultrasweetroute')
+      .end((error, response) => {
+        response.should.have.status(404)
+        response.text.should.equal('<!DOCTYPE html><html><head><title></title></head><body><h1>Not Found</h1><h2></h2><pre></pre></body></html>');
+      });
+    });
+  });
+
+  describe('yung API routes', () => {
+    describe('GET /api/v1/sharks', () => {
+      let cool = 'suh'
+      cool.should.equal('suh')
+    })
   })
-})
+});
