2656c30f711859911c5b01173240a660b5618e20
Charles Dunn <dunnc1551@gmail.com>
2017-06-05 13:07:56 -0600

    Add icon to each species object and render shark svgs.

--- client/src/components/controls/species.js
+++ client/src/components/controls/species.js
@@ -1,15 +1,29 @@
 import React from 'react';
 import { SpecieButton } from '../button/specieButton';
+import white from '../../images/white.svg';
+import blue from '../../images/blue.svg';
+import bull from '../../images/bull.svg';
+import blacktip from '../../images/blacktip.svg';
+import hammerhead from '../../images/hammerhead.svg';
+import mako from '../../images/mako.svg';
+import tiger from '../../images/tiger.svg';
+import whale from '../../images/whale.svg';
+import ship from '../../images/ship.svg';
+import turtle from '../../images/turtle.svg';
+
+const speciesIcons = [white, tiger, mako, blue, ship, blacktip, hammerhead, bull, blacktip, turtle, whale];
 
 export const SpeciesPanel = ({ species, handleClick }) => {
 
   const renderSpeciesBtns = () => {
-    
+    species = species.map((specie, i) => { return { name: specie, icon: speciesIcons[i] } });
+    console.log(species);
     return species.map((specie, i) =>
       <SpecieButton
         key={i}
-        specie ={ specie }
+        specie ={ specie.name }
         handleClick= { handleClick }
+        icon={ specie.icon }
       />
     )
   }
