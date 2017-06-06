1458c56314a122227dfe91bcb3bd04c6211dd0e1
Hugh Morrison <hmorri32@gmail.com>
2017-06-06 10:50:14 -0600

    move HOC to seperate folder/file, refactor MARKER in APP to SharkMArker

---
+++ client/src/components/contextProvider/ContextProvider.js
@@ -0,0 +1,21 @@
+import React, { Component, PropTypes } from 'react';
+
+export function withContext(WrappedComponent, context){
+
+  class ContextProvider extends React.Component {
+    getChildContext() {
+      return context;
+    }
+
+    render() {
+      return <WrappedComponent {...this.props} />
+    }
+  }
+
+  ContextProvider.childContextTypes = {};
+  Object.keys(context).forEach(key => {
+    ContextProvider.childContextTypes[key] = React.PropTypes.any.isRequired;
+  });
+
+  return ContextProvider;
+}
