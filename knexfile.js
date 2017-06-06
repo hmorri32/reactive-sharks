2e03c0a1cfbb29597321a20f5692d4aebb87be7d
Hugh Morrison <hmorri32@gmail.com>
2017-06-06 13:31:41 -0600

    heroku and knex config

--- knexfile.js
+++ knexfile.js
@@ -1,7 +1,6 @@
-// Update with your config settings.
+/* eslint-disable */
 
 module.exports = {
-
   development: {
     client: 'pg',
     connection: 'postgres://localhost/sharks',
@@ -22,6 +21,18 @@ module.exports = {
     },
     seeds: {
       directory: './db/seeds/test'
+    },
+    useNullAsDefault: true
+  },
+
+  production: {
+    client: 'pg',
+    connection: process.env.DATABASE_URL + `?ssl=true`,
+    migrations: {
+      directory: './db/migrations'
+    },
+    seeds: {
+      directory: './db/seeds/dev'
     },
     useNullAsDefault: true
   }
