d56eef57c1115de93b62dd4854720cc666e8d959
Hugh Morrison <hmorri32@gmail.com>
2017-06-06 14:32:12 -0600

    torching view engine. sorry jade.

--- app.js
+++ app.js
@@ -18,24 +18,10 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'client')));
 
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'jade');
+// app.set('views', path.join(__dirname, 'views'));
+// app.set('view engine', 'jade');
 
 app.use('/', index);
-
-app.use((req, res, next) => {
-  const err = new Error('Not Found');
-  err.status = 404;
-  next(err);
-});
-
-app.use((err, req, res, next) => {
-  res.locals.message = err.message;
-  res.locals.error   = req.app.get('env') === 'development' ? err : {};
-
-  res.status(err.status || 500);
-  res.render('error');
-});
 
 app.listen(app.get('port'), () => {
 /* eslint-disable no-console */
