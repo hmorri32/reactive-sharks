852b67d92cfa5d6fb7fd4e58798a574499bb5791
Hugh Morrison <hmorri32@gmail.com>
2017-06-07 11:33:31 -0600

    reorder stuff in knexfile

--- knexfile.js
+++ knexfile.js
@@ -4,13 +4,13 @@ module.exports = {
   development: {
     client: 'pg',
     connection: 'postgres://localhost/sharks',
-    useNullAsDefault: true,
     migrations: {
       directory: './db/migrations'
     },
     seeds: {
       directory: './db/seeds/dev'
-    }
+    },
+    useNullAsDefault: true,
   },
 
   test: {
